60aa3475b36de34537906bd4e04a616f873848c8
muyiwa olatunji <lrdmuyi85@gmail.com>
2025-08-15 06:20:22 +0100

    Stage 5

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask_mail import Mail, Message
 import sqlite3
 import pandas as pd
